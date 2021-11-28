9559551d1f41cfd57cb57e184bcbcafdf2d14ae1
InfiniteC0re <inf.core0@gmail.com>
2021-11-28 20:09:06 +0500

    removed unused steamworks module

--- node_modules/steamworks/index.js
+++
@@ -1,8 +0,0 @@
-var steamworks;
-
-if(process.arch == "x64")
-    steamworks = require('./steamworks-binding-x64.node');
-else
-    steamworks = require('./steamworks-binding-ia32.node');
-
-module.exports = steamworks;
